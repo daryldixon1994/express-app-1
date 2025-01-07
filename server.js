e5d223ed9e55e0e06b621372e82240bd793232a6
daryldixon1994 <kmohamedanouar@gmail.com>
2025-01-07 17:22:49 +0100

    second

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // create a web server with express
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const path = require("path");
 const PORT = 5000 || process.env.PORT;
+const URI_PWD = process.env.URI_PWD;
 
+const client = new MongoClient(
+  `mongodb+srv://gomycode:${URI_PWD}@cluster0.xu8jhiq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+);
 // buit in middleware
 // global middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //   end point
@@ -21,6 +28,29 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   //   end point
   res.sendFile(path.resolve("pages", "contact.html"));
+});
+app.get("/register", (req, res) => {
+  //   end point
+  res.sendFile(path.resolve("pages", "register.html"));
+});
+
+app.post("/send", (req, res) => {
+  res.sendFile(path.resolve("pages", "success.html"));
+});
+
+app.post("/register-user", async (req, res) => {
+  try {
+    // console.log(req.body);
+    await client.connect();
+    const db = client.db("company");
+    const collection = db.collection("users");
+    await collection.insertOne(req.body);
+    res.status(201).sendFile(path.resolve("pages", "success.html"));
+  } catch (error) {
+    res.status(400).send("Error");
+  } finally {
+    await client.close();
+  }
 });
 
 app.listen(PORT, (err) => {
